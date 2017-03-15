700c1f12ad38203fd7a3362d04c27f05351e934d
jie <jie5454@gmail.com>
2017-03-15 14:33:13 +0800

    alter code

--- decision_trees/classify_dt.py
+++ decision_trees/classify_dt.py
@@ -1,7 +1,6 @@
 def classify(features_train, labels_train):
-    ### your code goes here--should return a trained decision tree classifer
-
     from sklearn import tree
-    clf = tree.DecisionTreeClassifier()
+    # clf = tree.DecisionTreeClassifier()
+    clf = tree.DecisionTreeClassifier(min_samples_split=50)
     clf = clf.fit(features_train, labels_train)
     return clf
--- decision_trees/dt_main.py
+++ decision_trees/dt_main.py
@@ -9,15 +9,11 @@ from terrain_data.prep_terrain_data import makeTerrainData
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
-### the classify() function in classifyDT is where the magic
-### happens--fill in this function in the file 'classifyDT.py'!
 clf = classify(features_train, labels_train)
 
 predict = clf.predict(features_test)
 
 print calculate_accuracy(labels_test, predict)
-
-#### grader code, do not modify below this line
 
 prettyPicture(clf, features_test, labels_test)
 output_image("test.png", "png", open("test.png", "rb").read())
